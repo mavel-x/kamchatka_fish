4c77827f004c191309c554a0630314fcf6d48381
mavel <p.m.berdnikov@gmail.com>
2023-05-14 14:18:10 +0300

    simplify token management

--- ep_api.py
+++ ep_api.py
@@ -1,61 +1,65 @@
+from dataclasses import dataclass
 from datetime import datetime
-from typing import Generator
 
 import requests
 
 
+@dataclass
+class ElasticPathToken:
+    token: str = None
+    expiration: int = 0
+    
+    @property
+    def is_expired(self):
+        return int(datetime.now().timestamp()) > self.expiration
+
+
 class ElasticPathClient:
-    token_generator: Generator
-
     def __init__(self, ep_client_id: str, ep_client_secret: str):
-        self.token_generator = self._token_generator(ep_client_id, ep_client_secret)
+        self.client_id = ep_client_id
+        self.client_secret = ep_client_secret
+        self._token = ElasticPathToken()
 
-    @staticmethod
-    def _get_access_token(client_id, client_secret):
+    @property
+    def token(self):
+        if self._token.is_expired:
+            self._refresh_token()
+        return self._token.token
+        
+    def _refresh_token(self):
         access_url = 'https://api.moltin.com/oauth/access_token'
         data = {
             'grant_type': 'client_credentials',
-            'client_id': client_id,
-            'client_secret': client_secret,
+            'client_id': self.client_id,
+            'client_secret': self.client_secret,
         }
         response = requests.post(access_url, data=data)
         response.raise_for_status()
-        return response.json()
-
-    def _token_generator(self, ep_client_id, ep_client_secret):
-        """Generates a new Elastic Path token and automatically refreshes it on expiration.
-        """
-        token = None
-        expires_at = 0
-        while True:
-            if int(datetime.now().timestamp()) > expires_at:
-                token_response = self._get_access_token(ep_client_id, ep_client_secret)
-                token = token_response['access_token']
-                expires_at = token_response['expires']
-            yield token
+        token_response = response.json()
+        self._token = ElasticPathToken(
+            token=token_response['access_token'],
+            expiration=token_response['expires'],
+        )
 
     def get_all_products(self):
-        access_token = next(self.token_generator)
         products_url = 'https://api.moltin.com/pcm/products'
-        headers = {'Authorization': f'Bearer {access_token}'}
+        headers = {'Authorization': f'Bearer {self.token}'}
         response = requests.get(products_url, headers=headers)
         response.raise_for_status()
         products_raw = response.json()
         return products_raw['data']
 
     def get_product(self, product_id):
-        access_token = next(self.token_generator)
         url = f'https://api.moltin.com/pcm/products/{product_id}'
-        headers = {'Authorization': f'Bearer {access_token}'}
+        headers = {'Authorization': f'Bearer {self.token}'}
         response = requests.get(url, headers=headers)
         response.raise_for_status()
         product_raw = response.json()
         return product_raw['data']
 
     def _get_image_url(self, image_id):
-        access_token = next(self.token_generator)
         url = f'https://api.moltin.com/v2/files/{image_id}'
-        headers = {'Authorization': f'Bearer {access_token}'}
+        headers = {'Authorization': f'Bearer {self.token}'}
         response = requests.get(url, headers=headers)
         response.raise_for_status()
         return response.json()['data']['link']['href']
@@ -69,20 +73,18 @@ class ElasticPathClient:
         return image_url
 
     def get_cart_items(self, customer_id):
-        access_token = next(self.token_generator)
         url = f'https://api.moltin.com/v2/carts/{customer_id}/items'
         headers = {
-            'Authorization': f'Bearer {access_token}',
+            'Authorization': f'Bearer {self.token}',
         }
         response = requests.get(url, headers=headers)
         response.raise_for_status()
         return response.json()
 
     def add_item_to_cart(self, customer_id, product_id, quantity):
-        access_token = next(self.token_generator)
         url = f'https://api.moltin.com/v2/carts/{customer_id}/items'
         headers = {
-            'Authorization': f'Bearer {access_token}',
+            'Authorization': f'Bearer {self.token}',
             'Content-Type': 'application/json',
         }
         payload = {
@@ -97,20 +99,18 @@ class ElasticPathClient:
         return response.json()
 
     def delete_cart_item(self, customer_id, product_id):
-        access_token = next(self.token_generator)
         url = f'https://api.moltin.com/v2/carts/{customer_id}/items/{product_id}'
         headers = {
-            'Authorization': f'Bearer {access_token}',
+            'Authorization': f'Bearer {self.token}',
         }
         response = requests.delete(url, headers=headers)
         response.raise_for_status()
         return response.json()
 
     def create_customer(self, full_name, email):
-        access_token = next(self.token_generator)
         url = f'https://api.moltin.com/v2/customers'
         headers = {
-            'Authorization': f'Bearer {access_token}',
+            'Authorization': f'Bearer {self.token}',
             'Content-Type': 'application/json',
         }
         payload = {
